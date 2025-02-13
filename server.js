865f14455676f574199406c1e6dd9e5d19227895
Роман Шухевич <roman.berezdetskyi@megogo.net>
2025-02-13 20:11:38 +0200

    init

---
+++ server.js
@@ -0,0 +1,37 @@
+import http from 'node:http';
+import pg from 'pg';
+
+const client = new pg.Client({
+  host     : process.env.DB_HOSTNAME,
+  user     : process.env.DB_USERNAME,
+  password : process.env.DB_PASSWORD,
+  port     : process.env.DB_PORT,
+  database : process.env.DB_DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const server = http.createServer((req, res) => {
+  res.end('HELLO ROMAN');
+});
+
+async function main() {
+  try {
+    await client.connect();
+    await client.query(`
+CREATE TABLE IF NOT EXISTS app_user (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(450) NOT NULL
+)`);
+  
+    console.log('CONNECTION DONE !!!')
+  
+    server.listen(process.env.PORT);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main();
